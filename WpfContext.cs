2a6b49eb01f420a83525ccbb026cab347686ac19
rihanfarih <55140308+rihanfarih@users.noreply.github.com>
2022-03-09 21:35:02 +0700

    push

---
+++ WpfContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MySql.Data.EntityFramework;
+
+namespace WpfApp2
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    class WpfContext : DbContext
+    {
+        public DbSet<ItemPenjualan> DaftarItemPenjualan { get; set; }
+    }
+}
